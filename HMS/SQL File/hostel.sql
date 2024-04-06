52d9b651baee0f7e91d0e5978e4a172a8fb39555
allieah <115617110+allieah@users.noreply.github.com>
2024-04-06 23:24:51 +0530

    Add files via upload

---
+++ HMS/SQL File/hostel.sql
@@ -0,0 +1,264 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hostel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updation_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `email`, `password`, `reg_date`, `updation_date`) VALUES
+(1, 'admin', 'admin@gmail.com', 'admin', '2024-01-31 20:31:45', '2024-04-4');
+
+-- --------------------------------------------------------
+
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
+  `course_code` varchar(255) DEFAULT NULL,
+  `course_sn` varchar(255) DEFAULT NULL,
+  `course_fn` varchar(255) DEFAULT NULL,
+  `posting_date` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `course_code`, `course_sn`, `course_fn`, `posting_date`) VALUES
+(1, 'B10992', 'B.Tech', 'Bachelor  of Technology', '2024-04-4 19:31:42'),
+
+(2, 'BSC12', 'BSC', 'Bachelor  of Science', '2024-4-4 19:31:42'),
+(3, 'BC36356', 'BCA', 'Bachelor Of Computer Application', '2024-04-4 19:31:42'),
+(4, 'MCA565', 'MCA', 'Master of Computer Application', '2024-04-4 19:31:42'),
+(5, 'MBA75', 'MBA', 'Master of Business Administration', '2024-04-4 19:31:42');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `id` int(11) NOT NULL,
+  `roomno` int(11) DEFAULT NULL,
+  `seater` int(11) DEFAULT NULL,
+  `feespm` int(11) DEFAULT NULL,
+  `foodstatus` int(11) DEFAULT NULL,
+  `stayfrom` date DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
+  `course` varchar(500) DEFAULT NULL,
+  `regno` int(11) DEFAULT NULL,
+  `firstName` varchar(500) DEFAULT NULL,
+  `middleName` varchar(500) DEFAULT NULL,
+  `lastName` varchar(500) DEFAULT NULL,
+  `gender` varchar(250) DEFAULT NULL,
+  `contactno` bigint(11) DEFAULT NULL,
+  `emailid` varchar(500) DEFAULT NULL,
+  `egycontactno` bigint(11) DEFAULT NULL,
+  `guardianName` varchar(500) DEFAULT NULL,
+  `guardianRelation` varchar(500) DEFAULT NULL,
+  `guardianContactno` bigint(11) DEFAULT NULL,
+  `corresAddress` varchar(500) DEFAULT NULL,
+  `corresCIty` varchar(500) DEFAULT NULL,
+  `corresState` varchar(500) DEFAULT NULL,
+  `corresPincode` int(11) DEFAULT NULL,
+  `pmntAddress` varchar(500) DEFAULT NULL,
+  `pmntCity` varchar(500) DEFAULT NULL,
+  `pmnatetState` varchar(500) DEFAULT NULL,
+  `pmntPincode` int(11) DEFAULT NULL,
+  `postingDate` timestamp NULL DEFAULT current_timestamp(),
+  `updationDate` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `registration`
+--
+
+INSERT INTO `registration` (`id`, `roomno`, `seater`, `feespm`, `foodstatus`, `stayfrom`, `duration`, `course`, `regno`, `firstName`, `middleName`, `lastName`, `gender`, `contactno`, `emailid`, `egycontactno`, `guardianName`, `guardianRelation`, `guardianContactno`, `corresAddress`, `corresCIty`, `corresState`, `corresPincode`, `pmntAddress`, `pmntCity`, `pmnatetState`, `pmntPincode`, `postingDate`, `updationDate`) VALUES
+(2, 10, 3, 5000, 1, '2024-03-10', 6, 'Bachelor  of Technology', 10806121, 'Rita', '', 'dessai', 'female', 1234567890, 'ak@gmail.com', 1236547890, 'ABC', 'mother', 98756320000, 'ABC 12345 XYZ Street', 'Maharashtra', 'xxx', 110001, 'ABC 12345 XYZ Street', 'xxx', 'xxx', 110001, '2024-04-04 14:58:26', NULL),
+(3, 12, 2, 7000, 1, '2024-04-01', 12, 'Bachelor  of Science', 108061233, 'Neha', '', 'sharma', 'female', 1425362514, 'hohn@gmail.com', 456312058, 'XYZ', 'father', 1234567890, '123 Xyz apartment ', 'Maharashtra', 'yyy', 110001, '123 Xyz apartment ', 'yyy', 'yyy', 110001, '2024-04-4 05:42:03', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `id` int(11) NOT NULL,
+  `seater` int(11) DEFAULT NULL,
+  `room_no` int(11) DEFAULT NULL,
+  `fees` int(11) DEFAULT NULL,
+  `posting_date` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `rooms`
+--
+
+INSERT INTO `rooms` (`id`, `seater`, `room_no`, `fees`, `posting_date`) VALUES
+(1, 5, 100, 8000, '2024-04-4 22:45:43'),
+(2, 2, 201, 6000, '2024-04-4 22:45:43'),
+(3, 2, 200, 6000, '2024-04-4 22:45:43'),
+(4, 3, 112, 4000, '2024-04-4 22:45:43'),
+(5, 5, 132, 2000, '2024-04-4 22:45:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `states`
+--
+
+CREATE TABLE `states` (
+  `id` int(11) NOT NULL,
+  `State` varchar(150) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `states`
+--
+
+INSERT INTO `states` (`id`, `State`) VALUES
+(1, 'Andaman and Nicobar Island (UT)'),
+(2, 'Andhra Pradesh'),
+(3, 'Arunachal Pradesh'),
+(4, 'Assam'),
+(5, 'Bihar'),
+(6, 'Chandigarh (UT)'),
+(7, 'Chhattisgarh'),
+(8, 'Dadra and Nagar Haveli (UT)'),
+(9, 'Daman and Diu (UT)'),
+(10, 'Delhi (NCT)'),
+(11, 'Goa'),
+(12, 'Gujarat'),
+(13, 'Haryana'),
+(14, 'Himachal Pradesh'),
+(15, 'Jammu and Kashmir'),
+(16, 'Jharkhand'),
+(17, 'Karnataka'),
+(18, 'Kerala'),
+(19, 'Lakshadweep (UT)'),
+(20, 'Madhya Pradesh'),
+(21, 'Maharashtra'),
+(22, 'Manipur'),
+(23, 'Meghalaya'),
+(24, 'Mizoram'),
+(25, 'Nagaland'),
+(26, 'Odisha'),
+(27, 'Puducherry (UT)'),
+(28, 'Punjab'),
+(29, 'Rajastha'),
+(30, 'Sikkim'),
+(31, 'Tamil Nadu'),
+(32, 'Telangana'),
+(33, 'Tripura'),
+(34, 'Uttarakhand'),
+(35, 'Uttar Pradesh'),
+(36, 'West Bengal');
+
+-- --------------------------------------------------------
+
+--
+
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `room_no` (`room_no`);
+
+--
+-- Indexes for table `states`
+--
+ALTER TABLE `states`
+  ADD PRIMARY KEY (`id`);
+
+
+
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `states`
+--
+ALTER TABLE `states`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ HMS/admin/includes/header.php
@@ -0,0 +1,10 @@
+<div class="brand clearfix">
+		<a href="#" class="logo" style="font-size:16px; color:#fff;">Hostel Management System</a>
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		<ul class="ts-profile-nav">
+			<li class="ts-account">
+				
+				
+			</li>
+		</ul>
+	</div>
